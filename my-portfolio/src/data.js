ff10ba603c08ea56d35dccda1d21a2a32e4165af
Dimitri Vengersky <116322996+Vengerrr@users.noreply.github.com>
2023-03-29 14:22:16 +0900

    upload

---
+++ my-portfolio/src/data.js
@@ -0,0 +1,19 @@
+export const projects = [
+  {
+    title: "CheckList",
+    subtitle: "React Stack",
+    description:
+      "List of things you want to do",
+    image: "./project-1.gif",
+    link: "https://vengerrr.github.io/todolist/",
+  },
+  
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "CSS",
+  "Bootstrap",
+  "Command Line",
+];
