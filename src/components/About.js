772b83399d2c6563181dcd771cc79805b675808d
Dimitri Vengersky <116322996+Vengerrr@users.noreply.github.com>
2023-06-28 12:19:15 +0900

    Edited contsct form

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ export default function About() {
                 </a>
               </div>
             </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <div className="lg:max-w-lg md:w-1/2 w-5/6">
               <img
                 className="object-cover object-center rounded"
                 alt="hero"
